cc404fe5948a29c90b968081683eceae3a9dd48f
sagari <kazankidg@hotmail.co.jp>
2018-04-01 17:08:30 +0900

    Section1_add

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -10,7 +10,14 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
+            Section1 section1 = new Section1();
+
             Console.WriteLine("Hello World.");
+
+            int answer = section1.CalcCombination(100);
+
+            Console.WriteLine(answer);
+
             Console.ReadLine();
         }
     }
